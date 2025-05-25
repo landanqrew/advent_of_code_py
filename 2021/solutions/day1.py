444697f1d23d00e99325c6695a07403c788b6c0b
Rebecca Quartemont <rebeccaquartemont@rebeccas-mbp.lan>
2025-05-25 14:43:12 -0500

    completed 2021 day 1

--- 2021/solutions/day1.py
+++ 2021/solutions/day1.py
@@ -6,10 +6,19 @@ with open('2021/puzzles/day1.txt', 'r') as file:
 def decode_puzzle(puzzle: str) -> list[int]:
     return [int(num) for num in re.findall(r'\d+', puzzle)]
 
-def part1(puzzle: str) -> int:
+def solution(puzzle: str) -> dict[str: int]:
     nums: list[int] = decode_puzzle(puzzle)
-    return sum([1 for i, num in enumerate(nums) if i > 0 and nums[i - 1] < num])
+    p1: int = sum([1 for i, num in enumerate(nums) if i > 0 and nums[i - 1] < num])
+    p2: int = 0
+    l,r = 1,3
+    while r < len(nums):
+        if sum(nums[l:r + 1]) > sum(nums[l - 1:r]):
+            p2 += 1
+        r += 1
+        l += 1
+    return {"p1": p1, "p2": p2}
+
 
 
 if __name__ == '__main__':
-    print(f'p1: {part1(puzzle)}')
+    print(f'solution: {solution(puzzle)}')
